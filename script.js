b97d05fe03a11ba5a0b30d79054dc508baabb08c
andres <mury1827@gmail.com>
2019-10-07 11:58:23 +0100

    added javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = setdate;
+
+function setdate() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
+  today = dd + "/" + mm + "/" + yyyy;
+
+  document.getElementById("date").innerHTML = today;
+}
